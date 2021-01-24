fcb445e73277016748bd60b4b4f780ad9e5a2264
DevMarcosJesus <developermarcosjesus@gmail.com>
2021-01-23 21:59:13 -0300

    making general changes

--- models/Articles.js
+++ models/Articles.js
@@ -2,15 +2,33 @@ const Sequelize = require('sequelize');
 const db = require('../database/dbBlog');
 
 
+
+
 Articles = db.define('articles',{
     title: {
         type: Sequelize.STRING,
+        allowNull: false,
+
+
+    },slug:{
+        type: Sequelize.STRING,
+        allowNull: false,
+
+
     },
+
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+
+    }
 
 
 });
 
-//Articles.sync({force:true});
+
+Articles.sync({force:true});
 
 
 module.exports = Articles;
--- models/Categories.js
+++ models/Categories.js
@@ -2,18 +2,25 @@ const Sequelize = require('sequelize');
 const db = require('../database/dbBlog');
 
 
+
+
 Categories = db.define('categories', {
     title: {
         type: Sequelize.STRING,
         allowNull: false,
-    },
 
-    body: {
-        type: Sequelize.TEXT,
+
+    },slug:{
+        type: Sequelize.STRING,
         allowNull: false,
-    },
+
+        
+    }
+
+
 });
 
-//Categories.sync({force: true});
+
+Categories.sync({force: true});
 
 module.exports = Categories;
